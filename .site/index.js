874a721e4aa7aa815e676fd52eed793b0b78bb23
Robin <robin23jac@gmail.com>
2021-05-23 15:00:24 +0200

    Add sample daily site
    index.html is generated by the getfolders script and index.js will
    in the future be the compiled source code of a daily site frontend.

---
+++ .site/index.js
@@ -0,0 +1,28 @@
+
+const app = document.getElementById("app");
+
+let htmlString = "";
+
+for (let project of daily_projects) {
+    htmlString += `<h2>${project.name}</h2>`;
+
+
+    const types = Object.keys(project.branches);
+
+    for (const type of types) {
+        htmlString += `<h3>${type}</h3>`;
+        const branches = project.branches[type];
+
+        if (branches.length === 0) {
+            htmlString += `<p>No branch</p>`
+        } else {
+            htmlString += "<ul>";
+            for (const branch of branches) {
+                htmlString += `<li><a href="${project.url}/${branch}">${branch}</a></li>`;
+            }
+            htmlString += "</ul>";
+        }
+    }
+}
+
+app.insertAdjacentHTML("beforeend", htmlString);
