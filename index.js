357128e3265d48b2765856cce6ecf1d576f9ec51
robin jacobsson <robin.jacobsson@kebne.se>
2024-04-12 21:54:36 +0200

    Remove old stuff and rename to index

--- index.js
+++ index.js
@@ -3,38 +3,6 @@ const env = {
   OWNER: "robinjac",
   REPO: "daily-sites",
 };
-/* 
-let htmlString = "";
-
-fetch("https://robinjac.github.io/daily-sites/view_state.json", {
-  mode: "same-origin",
-})
-  .then((res) => res.json())
-  .then((json) => {
-    for (let project of json.projects) {
-      htmlString += `<h2>${project.name}</h2>`;
-
-      const types = Object.keys(project.branches);
-
-      for (const type of types) {
-        htmlString += `<h3>${type}</h3>`;
-        const branches = project.branches[type];
-
-        if (branches.length === 0) {
-          htmlString += `<p>No branch</p>`;
-        } else {
-          htmlString += "<ul>";
-          for (const branch of branches) {
-            htmlString += `<li><a href="/${json.host_repository}/${project.name}/${branch.slug}">${branch.name}</a> <span>${branch.date}</span></li>`;
-          }
-          htmlString += "</ul>";
-        }
-      }
-    }
-
-    app.insertAdjacentHTML("beforeend", htmlString);
-  });
- */
 
 const viewHtmlString = (view_state) => {
   let htmlString = "";
